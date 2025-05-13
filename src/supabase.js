204831ba38e8bd8cd1c13a3d687b6867dcf983c6
NewChoreographicDesign <59749463+NewChoreographicDesign@users.noreply.github.com>
2025-05-13 21:56:07 +0200

    Eerste versie van Raket op Toilet app

---
+++ src/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://xjyrmpdhnnovsylolfgk.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqeXJtcGRobm5vdnN5bG9sZmdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNjU3NDEsImV4cCI6MjA2Mjc0MTc0MX0.hUtm8UlzkdD7MYlqRWxVXnR9OlXg_T1USQz2x8FAsh4';
+export const supabase = createClient(supabaseUrl, supabaseKey);
