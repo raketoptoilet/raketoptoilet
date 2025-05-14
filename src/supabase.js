c96ed62024897eb72970d86166e12985bc4759b9
raketoptoilet <59749463+NewChoreographicDesign@users.noreply.github.com>
2025-05-14 10:54:30 +0200

    PWA manifest toegevoegd

--- src/supabase.js
+++ src/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xjyrmpdhnnovsylolfgk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhqeXJtcGRobm5vdnN5bG9sZmdrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcxNjU3NDEsImV4cCI6MjA2Mjc0MTc0MX0.hUtm8UlzkdD7MYlqRWxVXnR9OlXg_T1USQz2x8FAsh4';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
